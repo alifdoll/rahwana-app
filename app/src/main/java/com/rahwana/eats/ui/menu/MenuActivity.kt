da3601cb8ca9945b6ea6a2405712f173d6248f05
alif <alifianfajarpratama@gmail.com>
2021-08-24 19:30:22 +0700

    Initial Commit

---
+++ app/src/main/java/com/rahwana/eats/ui/menu/MenuActivity.kt
@@ -0,0 +1,12 @@
+package com.rahwana.eats.ui.menu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rahwana.eats.R
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
